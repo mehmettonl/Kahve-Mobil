3fa905ae3e60ec31df73191109b71485402ce8c8
mehmettonl <onalmehmet30@gmail.com>
2024-03-29 02:41:03 +0300

    ilk commit

---
+++ components/src/Loading.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View, ActivityIndicator, Pressable } from 'react-native';
+import React from 'react';
+
+const Loading = (props) => {
+  return (
+    <View style={styles.container}>
+      <Pressable 
+     onPress={()=> props.changeIsLoading()}
+     style = {[{},styles.closeButtonContainer]}>
+      <Text style ={styles.closeButton}>Hello</Text>
+      </Pressable>
+      <ActivityIndicator size={'large'} color={'white'} />
+      <Text style={styles.loginText}>{props.name}Loading...</Text>
+    </View>
+  );
+};
+
+export default Loading;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'white',
+    marginTop: 20,
+  },
+  closeButtonContainer:{
+  backgroundColor:'black',
+  width:50,
+  height:50,
+  borderRadius:50,
+  alignItems:"center",
+  justifyContent:'center',
+  position:'absolute',
+  top:50,
+  right: 30,
+
+  },  
+  closeButton: {
+    color:'#7fff00',
+    fontWeight:'bold',
+    fontSize:16,
+  },
+});
